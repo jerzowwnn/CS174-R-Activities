58046cc3c81e323524788c18b592d1bd899a0461
Jeraldo0803 <108305413+Jeraldo0803@users.noreply.github.com>
2024-03-06 21:56:21 +0800

    Added FA1.2 R and CSV
    Added R source code and CSV data

---
+++ FA1.2/CS174-R-FA1.2.R
@@ -0,0 +1,20 @@
+
+library(readr)
+library(ggplot)
+
+setwd("C:\\Users\\Justin\\Desktop\\R-Programming")
+bank_data<- read_delim("bank-full.csv", delim = ";")
+
+head(bank_data)
+
+str(bank_data)
+
+bank_data$y <- ifelse(bank_data$y == "no", 0, 1)
+
+model <- lm(bank_data$y ~ bank_data$balance + bank_data$duration, data=bank_data)
+
+summary(model)
+
+plot(model)
+
+
